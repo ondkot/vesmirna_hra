e3aa7878ece5cc5a472c9fdcb6fc47cfee46ea96
ondkot <81366620+ondkot@users.noreply.github.com>
2021-04-01 10:07:51 +0200

    Druhá revize - Loď létá, zatáčí a brzdí.

--- __main__.py
+++ __main__.py
@@ -1,7 +1,7 @@
 import pyglet
 import math
 ROTATION_SPEED = 4
-ACCELERATION = 2
+ACCELERATION = 10
 presed_keys = []
 window = pyglet.window.Window()
 class Spaceship:
@@ -11,18 +11,29 @@ class Spaceship:
 		self.x_speed = 0
 		self.y_speed = 0
 		self.rotation = 0
-		self.sprite = pyglet.sprite.Sprite(pyglet.image.load('images\\PNG\\playerShip1_blue.png'))
+		image = pyglet.image.load('images\\PNG\\playerShip1_blue.png')
+		image.anchor_x = image.width // 2
+		image.anchor_y = image.height // 2
+		self.sprite = pyglet.sprite.Sprite(image)
 		self.sprite.x = self.x
 		self.sprite.y = self.y
 		self.sprite.rotation = 90 - math.degrees(self.rotation)
+
 	def tick(self,dt):
 		if pyglet.window.key.UP in presed_keys:
 			self.x_speed += dt * ACCELERATION * math.cos(self.rotation)
 			self.y_speed += dt * ACCELERATION * math.sin(self.rotation)
+		if pyglet.window.key.DOWN in presed_keys:
+			self.x_speed -= dt * ACCELERATION * math.cos(self.rotation)
+			self.y_speed -= dt * ACCELERATION * math.sin(self.rotation)
 		if pyglet.window.key.LEFT in presed_keys:
 			self.rotation += dt * ROTATION_SPEED
+			self.x_speed += (32 * dt * ACCELERATION * math.cos(self.rotation))
+			self.y_speed += (32 * dt * ACCELERATION * math.sin(self.rotation))
 		if pyglet.window.key.RIGHT in presed_keys:
 			self.rotation -= dt * ROTATION_SPEED
+			self.x_speed += (32 * dt * ACCELERATION * math.cos(self.rotation))
+			self.y_speed += (32 * dt * ACCELERATION * math.sin(self.rotation))
 		self.x = self.x + dt * self.x_speed
 		self.y = self.y + dt * self.y_speed	
 		self.sprite.rotation = 90 - math.degrees(self.rotation)
@@ -39,10 +50,11 @@ def on_draw():
 
 @window.event
 def on_key_press(key_code,modifier):
-	if key_code == pyglet.window.key.UP or key_code == pyglet.window.key.LEFT or key_code == pyglet.window.key.RIGHT:
+	if key_code == pyglet.window.key.UP or key_code == pyglet.window.key.LEFT or key_code == pyglet.window.key.RIGHT or key_code == pyglet.window.key.DOWN:
 		presed_keys.append(key_code)
 @window.event
 def on_key_release(key_code,modifier):
 	if key_code in presed_keys:
 		del presed_keys[presed_keys.index(key_code)]
+pyglet.clock.schedule_interval(objects[0].tick,1/30)
 pyglet.app.run()
