14701beaa88f1be4086960d19061b694be13b42c
ondkot <81366620+ondkot@users.noreply.github.com>
2021-04-07 16:06:02 +0200

    Loď - kompletní Laser - robijí asteroidy

--- __main__.py
+++ __main__.py
@@ -4,14 +4,14 @@ import random
 ROTATION_SPEED = 4
 ACCELERATION = 10
 numbers = []
+window = pyglet.window.Window()
 path = "images\\PNG\\UI\\"
 for i in range(10):
 	file = "numeral" + str(i) + ".png"
 	numbers.append(pyglet.sprite.Sprite(pyglet.image.load(path + file)))
-	numbers[i].y = 425
+	numbers[i].y = window.height - 60
 	numbers[i].scale = 2
 presed_keys = []
-window = pyglet.window.Window()
 class globalni():
 	def __init__(self):
 		self.promena = ""
@@ -82,6 +82,7 @@ class Asteroid:
 			files = ["meteorBrown_big2.png","meteorGrey_big2.png","meteorBrown_big4.png","meteorGrey_big4.png","meteorBrown_big1.png","meteorGrey_big1.png","meteorBrown_big3.png","meteorGrey_big3.png","meteorBrown_med1.png","meteorGrey_med1.png","meteorBrown_med3.png","meteorGrey_med2.png","meteorBrown_small1.png","meteorGrey_small1.png","meteorBrown_small2.png","meteorGrey_small2.png","meteorGrey_tiny1.png","meteorBrown_tiny1.png","meteorGrey_tiny2.png","meteorBrown_tiny2.png"]
 			file = random.choice(files)
 		path = "images\\PNG\\Meteors\\"
+		self.file = file
 		image = pyglet.image.load(path + file)
 		image.anchor_x = image.width // 2
 		image.anchor_y = image.height // 2
@@ -155,7 +156,36 @@ class Laser:
 			asteroid_x = objects[i].x + asteroid_radius
 			asteroid_y = objects[i].y + asteroid_radius
 			if clash(asteroid_x,asteroid_y,spaceship_x,spaceship_y,asteroid_radius,spaceship_radius) and objects[i].__str__() == "Asteroid":
-				pass
+				file = objects[i].file[6:-4]
+				number = int(file[-1])
+				file = file[0:-1]
+				color = file[0:file.find("_")]
+				file = file[len(color):-1] + file[-1]
+				category = file[1:-1] + file[-1]
+				if number == 3:
+					number = 1
+				if number == 4:
+					number = 2
+
+				categorys = ["tiny","small","med","big"]
+				if category != "tiny":
+					category = categorys[categorys.index(category) - 1]
+					puvodni = objects[i]
+					objects[i] = Asteroid("meteor" + color + "_" + category + str(number) + ".png")
+					objects[i].x = puvodni.x
+					objects[i].y = puvodni.y
+					objects[i].rotation = puvodni.rotation
+					objects[i].x_speed = dt * ACCELERATION * math.cos(objects[i].rotation)
+					objects[i].y_speed = dt * ACCELERATION * math.sin(objects[i].rotation)
+					objects.append(Asteroid("meteor" + color + "_" + category + str(number) + ".png"))
+					objects[-1].x = puvodni.x
+					objects[-1].y = puvodni.y
+					objects[-1].rotation = puvodni.rotation
+					objects[-1].x_speed = dt * ACCELERATION * math.cos(objects[-1].rotation)
+					objects[-1].y_speed = dt * ACCELERATION * math.sin(objects[-1].rotation)
+				else:
+					objects[i] = Asteroid("blank.png")
+
 def call_tick(dt):
 	if lives.promena >= 0:
 		for i in objects:
@@ -189,15 +219,17 @@ objects = [Spaceship(),Asteroid("random"),Asteroid("random"),Asteroid("random"),
 @window.event
 def on_draw():
 	window.clear()
-	for i in objects:
-		i.sprite.draw()
 	if lives.promena >= 0:
 		if lives.promena <= 9:
 			numbers[lives.promena].draw()
+		else:
+			numbers[9].draw()
+		for i in objects:
+			i.sprite.draw()
 		x = pyglet.sprite.Sprite(pyglet.image.load("images\\PNG\\UI\\numeralX.png"))
 		life = pyglet.sprite.Sprite(pyglet.image.load("images\\PNG\\UI\\playerLife1_blue.png"))
-		x.y = 425
-		life.y = 420
+		x.y = window.height - 60
+		life.y = window.height - 60
 		x.x = 50
 		life.x = 100
 		x.scale = 2
@@ -205,7 +237,10 @@ def on_draw():
 		x.draw()
 		life.draw()
 	else:
-		GameOver().draw()
+		gameover = GameOver()
+		gameover.sprite.y = window.height // 3
+		gameover.sprite.x = window.width // 3
+		gameover.draw()
 
 
 @window.event
@@ -215,10 +250,16 @@ def on_key_press(key_code,modifier):
 	if key_code == pyglet.window.key.R:
 		objects[0] = Spaceship()
 		lives.promena = 9
+	if key_code == pyglet.window.key.ENTER:
+		if not window.fullscreen:
+			window = pyglet.window.Window(fullscreen = True)
+		else:
+			window = pyglet.window.Window(fullscreen = False)
 @window.event
 def on_key_release(key_code,modifier):
 	if key_code in presed_keys:
 		del presed_keys[presed_keys.index(key_code)]
+
 lives = globalni()
 lives.promena = 9
 pyglet.clock.schedule_interval(call_tick,1/30)
